e4e6f34beecf8eea32251a8d0526a109591cf522
marcelus B <mbenazea@gmail.com>
2022-03-15 02:43:12 -0400

    Adding/deleting users from a list

--- user-create.sh
+++ user-create.sh
@@ -8,13 +8,15 @@
 # Description: A program to create users and thier temporary password that should
 #************ should he change to their personalized password at their first log in.... 
 
-for i in USER{1..6}
-do
+for i in USER[1..6] 
 useradd ${i}
 passwd -d ${i}
 #welcome1
 echo "The following $i accounts were created successfully"
 done
 
-# to delet all the adder=d user
+# to delet all the added user
 # do userdel -r and run same script.
+# To add user from a file list sa at /tmp/usernames
+# for i in (cat /tmp/userneames)
+# useradd ${i}   # userdel -r ${i}
