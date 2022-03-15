ebbbaa775a04605e77b06659836d4b274397f81a
marcelus B <mbenazea@gmail.com>
2022-03-15 01:56:16 -0400

    useradd program in contruction

---
+++ user-create.sh
@@ -0,0 +1,17 @@
+
+
+#!/bin/bash
+
+#Author: Marcelus 
+# date Mach 15, 2022
+
+# Description: A program to create users and thier temporary password that should
+#************ should he change to their personalized password at their first log in.... 
+
+for i in USER{1..6}
+do
+useradd ${i}
+passwd -d ${i}
+#welcome1
+echo "The following $i accounts were created successfully"
+done
