0f65c1e5144c96aec131be4b57a77b000f11b9e0
marcelus B <mbenazea@gmail.com>
2022-03-15 00:21:22 -0400

    varaible reused example

---
+++ varaible_0.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+    #Author : marcelus B
+    #Date : march 14 2022
+
+## ---------- In this variable script I declare a variable that is reused multiple times.  -----------------
+
+#in our script,Marcelus is used 4 times, so good scripting practice requires us to create a variable to hold the Name. 
+# If the name is no longer the same, we will only have to change the value of our variable
+
+name=Marcelus
+
+echo "$name is a very hard working guy"
+echo "The manger says, he will hire $name in his new team wherever he goes"
+echo "$name likes to show off because the manger loves him"
+echo "$name will always be available when they need him"
